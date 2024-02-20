9ea8affb32765bfeae60a8a66dce953565e48eee
ajay solanki <ra7573769@gmail.com>
2024-02-20 10:15:34 +0530

    change mongodb string

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { MongoInternals } from 'meteor/mongo';
 import "../imports/startup/server/index";
 
 Meteor.startup(() => {
   // code to run on server at startup
+  const remote = new MongoInternals.RemoteCollectionDriver("mongodb+srv://ajaysolanki1106:wUFHu5D1gHjbeEga@cluster0.mqtrgau.mongodb.net/");
+
+  const newMongo = new Mongo.Collection('pitSubjects', { _driver: remote });
 });
